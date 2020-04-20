8b747074627a4966057ad4706a2fdc189cc2141a
Stephen McBride <stevemcbride3@gmail.com>
2020-04-20 06:21:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,10 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts "#{holiday.to_s.split.map {|word| word.capitalize}.join}: #{supplies.values}"
+      split_holiday_name = holiday.to_s.split
+      split_holiday_name.map {|word| word.capitalize}.join
+
+      puts "#{holiday.to_s.capitalize}: #{supplies.values}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
