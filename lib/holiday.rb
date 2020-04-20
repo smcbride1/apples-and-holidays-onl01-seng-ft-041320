e814430f4e28c59b3ac39d023e53a7c9f97fd3a8
Stephen McBride <stevemcbride3@gmail.com>
2020-04-20 06:54:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -98,7 +98,7 @@ def format_words(word)
 end
 
 puts format_words("test word")
-#all_supplies_in_holidays(holiday_hash)
+all_supplies_in_holidays(holiday_hash)
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
