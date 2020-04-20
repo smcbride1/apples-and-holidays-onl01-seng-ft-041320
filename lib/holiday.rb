5578952c3b586ecb1678e8c316b73499cd8cec8c
Stephen McBride <stevemcbride3@gmail.com>
2020-04-20 06:24:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,6 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
       capitalized_holiday = holiday.to_s.split.map {|word| word.capitalize}.join
       capitalized_supplies = supplies.map {|supply| supply.to_s.split.map{|word| word.capitalize}.join}
       puts "#{capitalized_holiday}: #{supplies.values}"
+    end
 end
 
 def all_holidays_with_bbq(holiday_hash)
