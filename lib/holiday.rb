955f8e372d38d4a22560eb871dbe2fed68c18a13
Stephen McBride <stevemcbride3@gmail.com>
2020-04-20 07:06:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -104,5 +104,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  bbq_included_holidays = []
+  holiday_hash.values.each do |holiday, supplies|
+    bbq_included_holidays.push(holiday.to_) if supplies.include?("BBQ")
 end
